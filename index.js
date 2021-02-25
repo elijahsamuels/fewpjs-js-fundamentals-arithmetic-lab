3d5d623d34552f23ed5f6d3a381be0dc0d6ee79f
Elijah Samuels <elijahsamuels@gmail.com>
2021-02-24 23:51:22 -0700

    commit tree 150403b64194eb52c40af9ddaacaa852977abc49

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num1 = 2
+const num2 = 31
+const num3 = 9
+const num4 = 5
+
+let multiply = num1*num2;
+
+let mod = num3%num4;
+
+ let max = Math.max(1,2,3,4,5,6,7,8,9,10,20 );
+ 
